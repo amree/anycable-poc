edc0c4f851ed460aac6886bfd3e3d3050b120c24
Amree Zaid <mohd.amree@gmail.com>
2025-03-04 08:57:28 +0800

    Add methods to sub and unsub

--- app/channels/price_channel.rb
+++ app/channels/price_channel.rb
@@ -1,11 +1,31 @@
 class PriceChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "price_channel"
-
     transmit({ message: "Subscribed!" })
   end
 
   def unsubscribed
     transmit({ message: "Unsubscribed!" })
+  end
+
+  def set_symbols(data)
+    symbols = Array(data["symbols"])
+
+    symbols.each do |symbol|
+      stream_name = "price_channel_#{symbol}"
+      stream_from stream_name
+
+      transmit({ message: "Subscribed to #{stream_name}!" })
+    end
+  end
+
+  def unset_symbols(data)
+    symbols = Array(data["symbols"])
+
+    symbols.each do |symbol|
+      stream_name = "price_channel_#{symbol}"
+      stop_stream_from stream_name
+
+      transmit({ message: "Unsubscribed from #{stream_name}!" })
+    end
   end
 end
